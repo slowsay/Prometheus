683b545f85487456fd27dc3952770a5474a8f4f5
Jack <slowsay@gmail.com>
2016-03-19 09:29:43 +0800

    Added files via upload

---
+++ docs/api.js
@@ -0,0 +1,23 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Prometheus",
+        "Prometheus.AssetsLoader",
+        "Prometheus.CanvasRender",
+        "Prometheus.DisplayObject",
+        "Prometheus.DisplayObjectContainer",
+        "Prometheus.Events",
+        "Prometheus.Graphics",
+        "Prometheus.Point",
+        "Prometheus.Rectangle",
+        "Prometheus.WebGlRender",
+        "Prometheus.requestRender",
+        "Prometheus.sprite",
+        "Prometheus.stage",
+        "Timefps"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
